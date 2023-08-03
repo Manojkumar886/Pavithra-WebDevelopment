79fa41322415238adecbe09d8afb4244ae9b5786
Manojkumar886 <maddymano434@gmail.com>
2023-08-03 11:43:45 +0530

    array methods  done and array create/list is done

---
+++ JAVA SCRIPT/Arraycrudl.js
@@ -0,0 +1,22 @@
+const colorsname=new Array("pink","blue","purple","black","white","gray","red","orange");
+
+let  listall=()=>
+{
+    let gettingcolors="";
+
+    colorsname.map((data)=>
+    {
+        gettingcolors+="<tr><td>"+data+"</td></tr>"
+    })
+    document.getElementById("mycolors").innerHTML="<table align='center'><thead><tr><th>Colors Names</th></tr></thead><tbody>"+gettingcolors+"</tbody></table>"
+}
+
+const creating=()=>
+{
+    let newvalue=document.getElementById("userid").value;
+
+    // colorsname.unshift(newvalue);
+    colorsname.push(newvalue);
+    alert(newvalue+" this color is added in my database")
+    listall()
+}
