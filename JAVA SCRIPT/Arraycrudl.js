6d1bb49fcba5bd1baba5c94c904ac0fbce48e739
Manojkumar886 <maddymano434@gmail.com>
2023-08-08 11:01:40 +0530

    crudl is done

--- JAVA SCRIPT/Arraycrudl.js
+++ JAVA SCRIPT/Arraycrudl.js
@@ -8,7 +8,7 @@ let  listall=()=>
     {
         gettingcolors+="<tr><td>"+data+"</td></tr>"
     })
-    document.getElementById("mycolors").innerHTML="<table align='center'><thead><tr><th>Colors Names</th></tr></thead><tbody>"+gettingcolors+"</tbody></table>"
+    document.getElementById("mycolors").innerHTML="<table align='center' cellspacing='20px'><thead><tr><th>Colors Names</th></tr></thead><tbody >"+gettingcolors+"</tbody></table>"
 }
 
 const creating=()=>
@@ -18,5 +18,50 @@ const creating=()=>
     // colorsname.unshift(newvalue);
     colorsname.push(newvalue);
     alert(newvalue+" this color is added in my database")
+    listall()
+}
+
+
+let updating=()=>
+{
+    let index=document.getElementById("userid").value;
+
+
+    let newvalue=prompt("please tell us your replace color name"+colorsname[index])
+
+    colorsname[index]=newvalue;
+    alert(colorsname[index]+" value replaced by "+newvalue)
+    listall()
+}
+
+let removing=()=>
+{
+    var deletedvalue=document.getElementById("userid").value;
+    alert("your value is deleted"+colorsname[deletedvalue])
+    delete colorsname[deletedvalue];
+   
+    // colorsname=colorsname.filter((colorsq)=>
+    // {
+    //     return colorsq!==deletedvalue;
+    // })
+    listall()
+}
+
+let searching=()=>
+{
+    let mycolor=document.getElementById("userid").value;
+
+    for(let pos=0;pos<colorsname.length;pos++)
+    {
+        if(colorsname[pos]==mycolor)
+        {
+            return pos;
+        }
+    }
+}
+
+function sorting()
+{
+    colorsname.sort();
     listall()
 }
